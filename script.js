11e9f263d0b81ee6d88e80effff79ba87af021b5
Hamisi_Juma <hamisig123@gmail.com>
2024-08-13 19:46:05 +0300

    Modified the look of the recipe page and added fonts

--- script.js
+++ script.js
@@ -42,3 +42,22 @@ document.addEventListener('click', function(event){
         hideMenu();
     }
 });
+
+// Toggle Dropdown on Click
+document.querySelector('.dropbtn').addEventListener('click',function(event){
+    event.preventDefault();
+    this.nextElementSibling.classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event){
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+}
+};
