b5bfe5ef47e6f2fcd4f65bc9bd5aba82675207c9
Hamisi_Juma <hamisig123@gmail.com>
2024-08-27 16:58:29 +0300

    Made the go up button

--- script.js
+++ script.js
@@ -67,3 +67,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+//Go up button
+let myUpButton = document.getElementById("goUpBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myUpButton.style.display = "block";
+    }else{
+        myUpButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    // document.body.scrollTop = 0; // For Safari
+    // document.documentElement.scrollTop = 0; // For Chrome,
+}
